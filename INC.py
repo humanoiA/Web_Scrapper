cba623a9307a056d36c41fa435e75081dae7f9f6
Shubhang <shubhang.sharma123@live.com>
2019-09-04 03:53:06 +0530

    First Commit

--- INC.py
+++ INC.py
@@ -21,11 +21,3 @@ for i in range(0,len(layers)):
         b=a.replace('">','--')
         c=b.split('--')
         print(c[0])
-'''for i in range(1,len(players)):
-    k=players[i].findAll("td",{"class":"cb-srs-stats-td"})
-    #for j in range(len(k)):
-    print("Name-"+k[0].text+"  /Matches-"+k[1].text+"   /Innings-"+k[2].text+"   /Runs-"+k[3].text+"   /Average-"+k[4].text)
-#tendulkar=players[1]
-#p=tendulkar.findAll("td",{"class":"cb-srs-stats-td"})
-#print(p[1].text)
-'''
